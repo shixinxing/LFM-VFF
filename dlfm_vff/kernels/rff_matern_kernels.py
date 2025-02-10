6a93caec06fe19506391a56da1854c920077ed7e
shixinxing <47957281+shixinxing@users.noreply.github.com>
2025-02-10 19:09:22 +0000

    Add files via upload

--- dlfm_vff/kernels/rff_matern_kernels.py
+++ dlfm_vff/kernels/rff_matern_kernels.py
@@ -38,30 +38,3 @@ class Matern_RFF_Kernel(RFFKernel):
         return super()._featurize(x, normalize=normalize)
 
 
-if __name__ == "__main__":
-    def test_kernel(nu=0.5, num_samples=1000, batch_shape=torch.Size([])):
-        matern_rff_kernel = Matern_RFF_Kernel(nu, num_samples, batch_shape)
-        matern_rff_kernel.lengthscale = torch.rand(*batch_shape, 1, 1)
-        matern_kernel = MaternKernel(nu=nu, batch_shape=batch_shape)
-        matern_kernel.lengthscale = matern_rff_kernel.lengthscale
-
-        x1 = torch.randn(*batch_shape, 4, 1) * 2
-        x2 = torch.randn(*batch_shape, 5, 1) * 3
-        K_truth = matern_kernel(x1, x2).to_dense()
-        K_rff = matern_rff_kernel(x1, x2).to_dense()
-
-        print(f"===== nu:{nu}, batch_shape:{batch_shape} =====")
-        print(f"x1 shape: {x1.shape}, x2 shape: {x2.shape}\n")
-        print(f"K_truth (shape: {K_truth.shape}): \n{K_truth}\n")
-        print(f"K_rff (shape: {K_rff.shape}): \n{K_rff}\n")
-        delta = K_truth - K_rff
-        assert torch.linalg.norm(K_truth) < 1e-5
-        relative_error = torch.linalg.matrix_norm(delta) / torch.linalg.matrix_norm(K_truth)
-        print(f"relative error: {relative_error}, max delta: {torch.max(torch.abs(delta))}\n")
-
-    torch.set_default_dtype(torch.float64)
-    # test_kernel(nu=0.5, num_samples=1000000, batch_shape=torch.Size([2]))
-    # test_kernel(nu=1.5, num_samples=100000, batch_shape=torch.Size([2]))
-    test_kernel(nu=2.5, num_samples=100000, batch_shape=torch.Size([2]))
-
-
